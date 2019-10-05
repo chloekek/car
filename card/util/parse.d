243c42ae23d0853eb52e40a58361896e37129ad0
Chloé <rightfold+ck@gmail.com>
2019-10-05 14:15:29 +0200

    Add mapping parser

--- card/util/parse.d
+++ card/util/parse.d
@@ -9,6 +9,8 @@ import std.range : ElementType, isForwardRange, iota, isInputRange;
 import std.traits : lvalueOf, rvalueOf;
 import std.typecons : Nullable, Tuple, tuple;
 
+import ascii = std.ascii;
+
 /+ -------------------------------------------------------------------------- +/
 
 alias Result = Nullable;
@@ -187,6 +189,64 @@ unittest
 
     assert (!result.isNull);
     assert (result.get == 'f');
+    assert (input == "oo"d);
+}
+
+/+ -------------------------------------------------------------------------- +/
+
+private
+struct But(alias F, P)
+{
+    private
+    alias f = unaryFun!F;
+
+    private
+    P parser;
+
+    template parse(I)
+    {
+        private
+        alias O = OutputType!(P, I);
+
+        Result!O parse(ref I i) const
+        {
+            auto r = parser.parse(i);
+            if (r.isNull)
+                return Result!O();
+            return Result!O(f(r.get));
+        }
+    }
+}
+
+/// Return a parser that applies the given function to the result of the given
+/// parser.
+auto but(alias F, P)(P parser)
+{
+    return But!(F, P)(parser);
+}
+
+///
+nothrow pure @nogc @safe
+unittest
+{
+    const parser = fail!int.but!(ascii.toUpper);
+    auto  input  = "foo"d;
+    const result = parser.parse(input);
+
+    assert (result.isNull);
+    assert (input == "foo"d);
+}
+
+///
+nothrow pure @nogc @safe
+unittest
+{
+    const parser = token.but!(ascii.toUpper);
+    auto  input  = "foo"d;
+    const result = parser.parse(input);
+
+    assert (!result.isNull);
+    assert (result.get == 'F');
     assert (input == "oo"d);
 }
 
